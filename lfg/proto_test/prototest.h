a70a4b0dca9cc986c8b79de6efa000d7e04459d2
LiuFG <1813683181@qq.com>
2019-07-30 05:39:10 +0800

    test proto

---
+++ lfg/proto_test/prototest.h
@@ -0,0 +1,11 @@
+//
+// Created by lfg on 19-7-30.
+//
+
+#ifndef HDMAPPROJECT_PROTOTEST_H
+#define HDMAPPROJECT_PROTOTEST_H
+
+#include "proto/constraint_set.pb.h"
+#include "ReadProtoFromTextFile/readproto.h"
+
+#endif //HDMAPPROJECT_PROTOTEST_H
