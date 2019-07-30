07c38ed1ae2c135202bdf1aed751251609fb93e3
LiuFG <1813683181@qq.com>
2019-07-30 22:32:58 +0800

    write prototxt

--- lfg/proto_test/src/prototest.cpp
+++ lfg/proto_test/src/prototest.cpp
@@ -12,7 +12,13 @@ int main(){
     bool read_state = protoio::ReadProtoFromTextFile(file_name, &constraint_set_config_);
 
     bool using_hsv_ = constraint_set_config_.using_hsv();
+    //更改元素
     constraint_set_config_.set_using_hsv(false);
+    //增加元素,改成repeated,新增一个armor_size
+    constraint_set_config_.add_armor_size()->set_width(11);
+    //删除元素
+    constraint_set_config_.clear_num();
+
     protoio::WriteProtoToTextFile(constraint_set_config_, new_file_name);
     float ok = constraint_set_config_.num();
     std::cout<< using_hsv_<<std::endl;
