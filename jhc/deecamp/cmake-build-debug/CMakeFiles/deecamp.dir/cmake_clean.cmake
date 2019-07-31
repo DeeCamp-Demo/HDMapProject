b45f3b3b33a0783e6c517da140d6577692855887
jhch1995 <jhch1995@mail.ustc.edu.cn>
2019-07-31 22:07:29 +0800

    update v1

---
+++ jhc/deecamp/cmake-build-debug/CMakeFiles/deecamp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/deecamp.dir/main.cpp.o"
+  "CMakeFiles/deecamp.dir/Utils.cc.o"
+  "deecamp.pdb"
+  "deecamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/deecamp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
